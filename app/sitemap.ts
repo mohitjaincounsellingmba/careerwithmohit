da92d91778b3b2d72410cd5d2acba34af4ad4769
“Mohit <advik.mohit.jain@gmail.com>
2026-03-08 15:03:48 +0530

    chore: add terms page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,6 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/inquiry',
     '/about',
     '/privacy',
+    '/terms',
   ].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date(),
