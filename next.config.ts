f5e388ddf86f3b5c98e21191f0ff0b2401923374
Mohit Jain <advik.mohit.jain@gmail.com>
2026-05-30 12:40:43 +0530

    chore: configure redirects for Google Search Console 404 URLs

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,122 @@ const nextConfig: NextConfig = {
         destination: '/mock-tests',
         permanent: true,
       },
+      // GSC 404 Redirects
+      {
+        source: '/best-mba-colleges-in-pune-2026',
+        destination: '/blog/best-mba-colleges-in-pune-2026',
+        permanent: true,
+      },
+      {
+        source: '/brochures/jaipuria-jaipur-2025.pdf',
+        destination: '/colleges/jaipuria-jaipur',
+        permanent: true,
+      },
+      {
+        source: '/brochures/bimtech-2025.pdf',
+        destination: '/colleges/bimtech-greater-noida',
+        permanent: true,
+      },
+      {
+        source: '/brochures/fms-2025.pdf',
+        destination: '/colleges/fms-delhi',
+        permanent: true,
+      },
+      {
+        source: '/brochures/greatlakes-gurgaon-2025.pdf',
+        destination: '/colleges/great-lakes-gurgaon',
+        permanent: true,
+      },
+      {
+        source: '/brochures/iift-2025.pdf',
+        destination: '/colleges/iift-delhi',
+        permanent: true,
+      },
+      {
+        source: '/brochures/iima-pgp-2025.pdf',
+        destination: '/colleges/iim-ahmedabad',
+        permanent: true,
+      },
+      {
+        source: '/brochures/iimb-2025.pdf',
+        destination: '/colleges/iim-bangalore',
+        permanent: true,
+      },
+      {
+        source: '/brochures/imik-2025.pdf',
+        destination: '/colleges/imi-kolkata',
+        permanent: true,
+      },
+      {
+        source: '/brochures/itm-2025.pdf',
+        destination: '/colleges/itm-mumbai',
+        permanent: true,
+      },
+      {
+        source: '/brochures/jaipuria-noida-2025.pdf',
+        destination: '/colleges/jaipuria-noida',
+        permanent: true,
+      },
+      {
+        source: '/brochures/mdi-2025.pdf',
+        destination: '/colleges/mdi-gurgaon',
+        permanent: true,
+      },
+      {
+        source: '/brochures/mnit-2025.pdf',
+        destination: '/colleges/mnit-jaipur',
+        permanent: true,
+      },
+      {
+        source: '/brochures/xime-2025.pdf',
+        destination: '/colleges/xime-bangalore',
+        permanent: true,
+      },
+      {
+        source: '/brochures/:path*',
+        destination: '/inquiry',
+        permanent: false,
+      },
+      {
+        source: '/blog/rvce-bangalore-management-quota',
+        destination: '/blog/rv-college-of-engineering-rvce-btech-admission-2026-fees-cutoff',
+        permanent: true,
+      },
+      {
+        source: '/cuet-ug-university-list-2026-citywise',
+        destination: '/blog/cuet-ug-university-list-2026-citywise',
+        permanent: true,
+      },
+      {
+        source: '/blog/top-bba-colleges-coimbatore-2026',
+        destination: '/blog/top-btech-colleges-coimbatore-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/how-to-',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/tools/instagram-followers-generator',
+        destination: '/tools/hashtag-generator',
+        permanent: true,
+      },
+      {
+        source: '/tools/cat-',
+        destination: '/tools/cat-mock-test',
+        permanent: true,
+      },
+      {
+        source: '/blog/top-btech-colleges-north-east',
+        destination: '/blog/top-10-engineering-colleges-india-2026',
+        permanent: true,
+      },
+      {
+        source: '/&',
+        destination: '/',
+        permanent: true,
+      },
     ];
   },
 };
